7c07ff63ed18cc36de46e6f02801a90f19ef061f
Praveen Mathew <praveen.mathew92@outlook.com>
2021-06-27 11:35:44 +0530

    Move notifying all users to a common function

--- chatroom/ChatRoom.py
+++ chatroom/ChatRoom.py
@@ -9,21 +9,23 @@ class ChatRoom:
         return self.users_with_notifiers.keys()
 
     def add_user(self, user):
-        notifiers = filter(None, self.users_with_notifiers.values())
-        for notifier in notifiers:
-            notifier.new_user_notification(user)
+        add_user_notification_sender = lambda notifier: notifier.new_user_notification(user)
+        self.__send_notification_to_all_users(add_user_notification_sender)
         self.users_with_notifiers[user] = None
 
     def remove_user(self, user):
-        notifiers = filter(None, self.users_with_notifiers.values())
-        for notifier in notifiers:
-            notifier.user_removed_notification(user)
         del self.users_with_notifiers[user]
+        remove_user_notifiaction_sender = lambda notifier: notifier.user_removed_notification(user)
+        self.__send_notification_to_all_users(remove_user_notifiaction_sender)
 
     def add_user_notifier(self, user, notifier: Notifier):
         self.users_with_notifiers[user] = notifier
 
     def send_message(self, user, message):
+        message_sender = lambda notifier: notifier.send_message(message)
+        self.__send_notification_to_all_users(message_sender)
+
+    def __send_notification_to_all_users(self, notifier_function_caller):
         notifiers = filter(None, self.users_with_notifiers.values())
         for notifier in notifiers:
-            notifier.send_message(message)
+            notifier_function_caller(notifier)
